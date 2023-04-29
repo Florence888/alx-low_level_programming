1949a2def71834e2b7d588f6ce994cf0be61483a
Florence <aliemwanta@gmail.com>
2023-04-28 19:41:09 -0700

    0-main.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ * Return: 1 for uppercase and 0 for anything else
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
+
